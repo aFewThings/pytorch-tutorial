a33830257b1f538d234ee1c27d2b1ee6c6740ec7
afewthings <gichanac@naver.com>
2021-04-20 00:40:46 +0900

    update
    - moving all snippets to 'personal-snippets' repo

--- snippets/_mask.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-src = torch.arange(9).float().reshape((3, 3))
-mask = torch.tensor([[True, True, True],
-                     [False, False, True],
-                     [True, False, True]])
-value = torch.randn(3, 3)
-src[mask] = value[mask]
-print(src)
